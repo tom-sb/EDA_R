1f0b06aeb5501be58404cb0236c568bd5b1bd4f9
tom-sb <fvillanuevanut@gmail.com>
2019-12-11 16:50:54 -0500

    create point and rect.
    Co-authored-by: tom-sb<fvillanuevanut@gmail.com>

--- rtree.py
+++ rtree.py
@@ -1,8 +1,37 @@
+class Point:
+	def __init__(self,_x,_y):
+		self.x = _x
+		self.y = _y
+
 
 class Rectangulo:
-	def __init__(self,x_min,y_min,x_max,y_max):
-		self.min_x = x_min
-		self.min_y = y_min
-		self.max_x = x_max
-		self.max_y = y_max
+	def __init__(self,left_t ,right_b):
+		self.left_top = left_t
+		self.right_bottom = right_b
+		self.height
+		self.width
+		self.area
+		
+
+
+
+
+
+
+
+class Branch:
+	def __init_(self):
+		self.rect = Rectangulo(x_min,)
+
+class Node:
+	def __init__():
+		count
+		level
+		branches[NODE_MAX]
+
+class Nodes_list:
+
+
+class Rtree:
+	def __init__():
 	
